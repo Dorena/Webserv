aa5d004b9e0415b908baf94ee039bc4115accd1e
dorena <johan.aslund.kdala@gmail.com>
2016-02-05 14:43:50 +0100

    Php shit

---
+++ Upgifter/mysql.php
@@ -0,0 +1,19 @@
+<?php
+
+
+
+DEFINE('DB_USER','root');
+DEFINE('DB_HOST','localhost');
+DEFINE('DB_PASSWORD','dank');
+DEFINE('DB_DATABASE','intro');
+		
+ 		
+$dbc = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
+
+
+
+
+
+
+
+?>
